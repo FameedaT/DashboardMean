7f8c4bc64b594c9b1b23d478a0ac95f0f2371b55
unknown <fameedat@ULTP_092.synerzipune.local>
2015-02-12 00:59:13 +0530

    View of Employees CRUD operations

--- app/models/employee.server.model.js
+++ app/models/employee.server.model.js
@@ -4,26 +4,61 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    relationship = require('mongoose-relationship');
 
 /**
  * Employee Schema
  */
 var EmployeeSchema = new Schema({
-	name: {
-		type: String,
-		default: '',
-		required: 'Please fill Employee name',
-		trim: true
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+
+    firstName: {
+        type: String,
+        trim: true,
+        default: '',
+        required: 'Please fill Employee First Name'
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        default: '',
+        required: 'Please fill Employee Last Name'
+    },
+    skills: [
+        {
+            type: String
+        }
+    ],
+    experience: {
+        type: Number
+    },
+    belongsTo: {
+        type: Schema.ObjectId,
+        ref: 'Organization',
+        childPath: 'members'
+    },
+    worksFor: [{
+        type: Schema.ObjectId,
+        ref: 'Project',
+        childPath: 'members'
+    }],
+    role: {
+        type: String,
+        enum: ['Junior Software Developer', 'Software Developer', 'Senior Software Developer', 'Junior QA Engineer', 'QA Engineer', 'Senior QA Engineer', 'Tech Lead', 'QA Lead', 'Engineering Manager', 'QA Manager', 'Architect', 'BU Head']
+    },
+    billable: {
+        type: Boolean
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
 });
+
+EmployeeSchema.plugin(relationship, {relationshipPathName: ['belongsTo', 'worksFor']});
 
 mongoose.model('Employee', EmployeeSchema);
